8c4799a1ffe3a969e74f0e91aa300f366681fa5a
darshana <darshanavadalia@gmail.com>
2023-07-10 16:44:26 +0530

    update 1st demo code

---
+++ Export.js
@@ -0,0 +1,41 @@
+
+
+module.exports = {
+    botId: 'st-fa3c2d6e-128d-5e18-a60a-eca34e4a9132',
+    botName: 'Demobot1',
+    
+    on_variable_update: function(requestId, data, callback) {
+        var event = data.eventType;
+        console.log("event----------->", event);       
+        
+        const { spawn } = require('child_process');
+
+        // Define the PowerShell script path and arguments
+        const scriptPath = 'E:/Darshana-infy/otherscript/export.ps1';
+        const scriptArgs = ['-ExecutionPolicy', 'Bypass', '-File', scriptPath];
+        
+        // Spawn a new PowerShell process
+        const powershellProcess = spawn('powershell.exe', scriptArgs);
+        
+        // Capture the output of the PowerShell script
+        powershellProcess.stdout.on('data', (data) => {
+          console.log(`PowerShell script output: ${data}`);
+        });
+        
+        // Handle any errors or completion of the PowerShell script
+        powershellProcess.on('error', (error) => {
+          console.error(`Error executing PowerShell script: ${error}`);
+        });
+        
+        powershellProcess.on('close', (code) => {
+          if (code === 0) {
+            console.log('PowerShell script executed successfully.');
+          } else {
+            console.error(`PowerShell script exited with code ${code}.`);
+          }
+        });
+        
+    
+    }
+};
+
