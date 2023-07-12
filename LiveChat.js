b940d6d4f6122c096d4c47a85b6c0a679eb0b965
darshana <darshanavadalia@gmail.com>
2023-07-12 12:03:04 +0530

    update code

--- LiveChat.js
+++ LiveChat.js
@@ -213,7 +213,7 @@ module.exports = {
     gethistory: gethistory
 };
 
-
+//save as backup
 //   var username = "darshana",
       //   password = "114ac5bd147dd660f1c8c0282c69c46331";
       //   const exurl = "http://"+username+":"+password+"@122.170.77.44:8003/job/Botkit%20Job/build?token=123";
