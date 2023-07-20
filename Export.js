93fdea7135f6e9147dbe957c49e959c9bb3a6888
darshana0406 <darshanavadalia04@gmail.com>
2023-07-20 20:02:33 +0530

    update code

--- Export.js
+++ Export.js
@@ -11,45 +11,57 @@ module.exports = {
         
 
         // //---------------------------Running Java File (For that we rquired Child process and cmd is "npm install child_process")
-        // const { exec } = require('child_process');
+        const { exec } = require('child_process');
+        // async function fetchUserData() {
+           exec('java -cp "C:\\Users\\gg\\AppData\\Roaming\\Code\\User\\workspaceStorage\\af7354454b49f903ff1000875a90235f\\redhat.java\\jdt_ws\\Botkit-Master-Demo_78b8d7eb\\bin;c:\\Users\\gg\\Downloads\\Java\\json-20230618.jar" BotJava', (error, stdout, stderr) => {
+          if (error) {
+            console.error('Error executing Java file:', error);
+            return;
+          }
 
-        // exec('java BotJava', (error, stdout, stderr) => {
-        //   if (error) {
-        //     console.error('Error executing Java file:', error);
-        //     return;
-        //   }
+          console.log('Java file output:', stdout);
+        });
+      // }
+      // fetchUserData();
+        
 
-        //   console.log('Java file output:', stdout);
-        // });
+         exec('git add . ', (error, stdout, stderr) => {
+          if (error) {
+            console.error('Error executing Git:', error);
+            return;
+          }
 
+          console.log('Git output:', stdout);
+        });
+        
 
        // //--------------------Running Powershell File 
-        const { spawn } = require('child_process');
+        // const { spawn } = require('child_process');
 
-        // Define the PowerShell script path and arguments
-        const scriptPath = 'DifferentBot.ps1'; 
-        const scriptArgs = ['-ExecutionPolicy', 'Bypass', '-File', scriptPath];
+        // // Define the PowerShell script path and arguments
+        // const scriptPath = 'DifferentBot.ps1'; 
+        // const scriptArgs = ['-ExecutionPolicy', 'Bypass', '-File', scriptPath];
         
-        // Spawn a new PowerShell process
-        const powershellProcess = spawn('powershell.exe', scriptArgs);
+        // // Spawn a new PowerShell process
+        // const powershellProcess = spawn('powershell.exe', scriptArgs);
         
-        // Capture the output of the PowerShell script
-        powershellProcess.stdout.on('data', (data) => {
-          console.log(`PowerShell script output: ${data}`);
-        });
+        // // Capture the output of the PowerShell script
+        // powershellProcess.stdout.on('data', (data) => {
+        //   console.log(`PowerShell script output: ${data}`);
+        // });
         
-        // Handle any errors or completion of the PowerShell script
-        powershellProcess.on('error', (error) => {
-          console.error(`Error executing PowerShell script: ${error}`);
-        });
+        // // Handle any errors or completion of the PowerShell script
+        // powershellProcess.on('error', (error) => {
+        //   console.error(`Error executing PowerShell script: ${error}`);
+        // });
         
-        powershellProcess.on('close', (code) => {
-          if (code === 0) {
-            console.log('PowerShell script executed successfully.');
-          } else {
-            console.error(`PowerShell script exited with code ${code}.`);
-          }
-        });
+        // powershellProcess.on('close', (code) => {
+        //   if (code === 0) {
+        //     console.log('PowerShell script executed successfully.');
+        //   } else {
+        //     console.error(`PowerShell script exited with code ${code}.`);
+        //   }
+        // });
         
         // // ----------- Direct trigger jenkins job
         //   var username = "darshana",
